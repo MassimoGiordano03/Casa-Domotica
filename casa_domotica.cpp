faedcb0167b9c69e4d14b53586d4f7bec5ad5100
MassimoGiordano03 <massimo.giord@gmail.com>
2020-11-05 09:20:49 +0100

    Impostato il lavoro sulla casa

---
+++ casa_domotica.cpp
@@ -0,0 +1,25 @@
+/**
+ * casa_domotica.cpp
+ * main file
+ */
+
+#include <Arduino.h>
+/**
+ * 1 -> Creare un file di testo con tutto ciò che ci serve e i nomi dei programmini che dobbiamo costruire
+ * 2 -> Creare dei file .ino in cui creiamo i programmini per gestire tutto separatamente
+ * 3 -> Una volta che i mini programmini sono funzionanti, creare delle librerie .cpp e .hpp che riproducano il funzionamento dei .ino e testarle singolarmente
+ * 4 -> Quando tutto funzionerà separatamente anche nella logica del compilatore, unire piano piano tutto e correggere gli errori
+ */
+
+// Per qualunque dubbio nei primi 2 passaggi, scrivimi che ci mettiamo d'accordo per sentirci su discord, così ti spiego tutto
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
+
